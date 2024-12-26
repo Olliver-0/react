04bd2b8ff1488a8e67f8526d2a2ace206d32ed89
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2024-12-26 11:10:58 -0300

    Aprendendo mais sobre React

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 const nome = 'Celso'
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Natã" />
       <SayMyName nome="Clarêncio" />
       <SayMyName nome={nome} />
       <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150" />
+      <List />
     </div>
   );
 };
